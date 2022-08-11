1abfd5772445de0e96d495da07c3cfc4a141ed97
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2022-08-12 00:10:17 +0200

    Add harness for the new section

--- src/pages/scala-intro.js
+++ src/pages/scala-intro.js
@@ -10,12 +10,12 @@ const IndexPage = ({ data }) =>
   <Layout>
     <Seo />
     <div>
-      <h1 class="title">📝 Blog Posts</h1>
+      <h1 class="title">📖 Introduction to Programming with Scala </h1>
       <ul id="blog-posts" class="posts">
         {
           data.allMarkdownRemark.nodes.map(post => {
             return(
-              <li><span class="is-family-monospace has-text-grey-light">{post.fields.date} &raquo;</span> <Link to={`/posts/${post.fields.slug}`}>{post.frontmatter.title}</Link></li>
+              <li><span class="is-family-monospace has-text-grey-light">&raquo;</span> <Link to={`/scala-intro/${post.fields.slug}`}>{post.frontmatter.title}</Link></li>
             )
           })
         }
@@ -25,7 +25,7 @@ const IndexPage = ({ data }) =>
 
 export const query = graphql`
   {
-    allMarkdownRemark(filter: {frontmatter: {section: {eq: null}}}, sort: {fields: fields___date, order: DESC}) {
+    allMarkdownRemark(filter: {frontmatter: {section: {eq: "scala-intro"}}}, sort: {fields: fields___slug, order: DESC}) {
       nodes {
         frontmatter {
           title
