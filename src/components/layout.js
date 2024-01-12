6c23fa2df612e80da604693021e16ca8bb2bbf8e
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2024-01-12 20:32:46 +0100

    Add Plausible tracking script

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,12 +9,25 @@ const Layout = ({ children }) => {
     <>
       <Helmet>
         <title>Anatolii's Programming Blog</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+          integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <script
+          defer
+          data-domain="akmetiuk.com"
+          src="https://plausible.cloud.akmetiuk.com/js/script.js"
+        ></script>
       </Helmet>
       <main class="has-background-light">
         <div class="columns is-gapless">
           <div class="column is-one-quarter is-hidden-mobile">
-            <div class="box m-2"><Sidebar /></div>
+            <div class="box m-2">
+              <Sidebar />
+            </div>
           </div>
           <div class="column is-three-quarters">
             <div class="box m-2">{children}</div>
@@ -25,7 +38,7 @@ const Layout = ({ children }) => {
         <p>© Anatolii Kmetiuk, 2016 - 2023</p>
       </footer>
     </>
-  )
+  );
 }
 
 export default Layout
